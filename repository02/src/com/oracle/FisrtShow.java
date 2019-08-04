168511aad7b574201b7e768841cf241baea188b3
Arwen <Arwen-light@outlook.com>
2019-08-04 22:54:17 +0800

    second modify to know

--- repository02/src/com/oracle/FisrtShow.java
+++ repository02/src/com/oracle/FisrtShow.java
@@ -5,11 +5,12 @@ public class FisrtShow {
     public static void main(String[] args) {
         System.out.println("show: find something is new  and to decide what is the best thing to you " );
     }
-
+ //  text  promate to send to anywhere
     public static  int sum(int a, int b){
         int result ;
         result = a + b;
         System.out.println(result);
+        System.out.println("good good study , day day up");
         return result;
     };
 }
