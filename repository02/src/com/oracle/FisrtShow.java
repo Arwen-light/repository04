ad409e01fcea4bebbc239823b9946b50396f77b7
Arwen <Arwen-light@outlook.com>
2019-08-04 22:35:40 +0800

    to create a project for test

---
+++ repository02/src/com/oracle/FisrtShow.java
@@ -0,0 +1,14 @@
+package com.oracle;
+
+public class FisrtShow {
+
+    public static void main(String[] args) {
+        System.out.println("show: find something is new  and to decide what is the best thing to you " );
+    }
+
+    public static  int sum(int a, int b){
+        int result ;
+        result = a + b;
+        return result;
+    };
+}
